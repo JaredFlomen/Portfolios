fd04203f056c589bd6605817f26334a7868c4ed6
Jared Flomen <jaredflomen@gmail.com>
2021-03-29 10:51:25 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
+  const [ticker, setTicker] = useState('');
+  const [portfolio, setPortfolio] = useState([]);
+  const options = {
+    method: 'GET',
+    url: 'https://alpha-vantage.p.rapidapi.com/query',
+    params: { function: 'GLOBAL_QUOTE', symbol: `${ticker}` },
+    headers: {
+      'x-rapidapi-key': '7d487c6018msh1781a0a2541286ap1434c3jsna8d90d7fdd8b',
+      'x-rapidapi-host': 'alpha-vantage.p.rapidapi.com',
+    },
+  };
+  const find = () => {
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data['Global Quote']['08. previous close']);
+      })
+      .catch(function (error) {
+        console.error(error.message);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type='text'
+        placeholder='Symbol'
+        onChange={e => setTicker(e.target.value)}
+      />
+      <button onClick={find}>Find</button>
     </div>
   );
 }
