a5e8f9fd80d8e48e05a5e7a2028064716cf2273f
Jared Flomen <jaredflomen@gmail.com>
2021-04-01 10:07:10 -0400

    Adding portfolio weight

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import lastPrice from './helpers/lastPrice';
 
 function App() {
   const [ticker, setTicker] = useState('');
+  const [weight, setWeight] = useState(0);
   const [portfolio, setPortfolio] = useState([
-    { ticker: 'AAPL', price: 100.12, shares: 1 },
-    { ticker: 'TSLA', price: 600.34, shares: 1 },
-    { ticker: 'SHOP', price: 1000, shares: 1 },
+    { ticker: 'AAPL', price: 100.12, weight: 10, shares: 1 },
+    { ticker: 'TSLA', price: 600.34, weight: 5, shares: 2 },
+    { ticker: 'SHOP', price: 1000, weight: 20, shares: 1 },
   ]);
   const updatePortfolio = () => {
     Promise.all(
@@ -25,7 +26,7 @@ function App() {
       setTicker('');
       return null;
     } else {
-      setPortfolio([...portfolio, { ticker, price, shares: 1 }]);
+      setPortfolio([...portfolio, { ticker, price, weight }]);
       setTicker('');
     }
   }
@@ -43,6 +44,12 @@ function App() {
         value={ticker}
         onChange={e => setTicker(e.target.value)}
       />
+      <input
+        type='number'
+        placeholder='Portfolio Weight'
+        value={weight}
+        onChange={e => setWeight(e.target.value)}
+      />
       <button onClick={addPosition}>Add Position</button>
       <button onClick={updatePortfolio}>Update All Positions</button>
       <div>Market value: {parseFloat(marketValue).toFixed(2)}</div>
@@ -53,6 +60,7 @@ function App() {
               key={index}
               ticker={item.ticker}
               price={item.price}
+              weight={item.weight}
               shares={item.shares}
             />
           );
--- src/components/Position.js
+++ src/components/Position.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import '../styles/position.css';
 
-function Position({ ticker, price, shares }) {
+function Position({ ticker, price, shares, weight }) {
   return (
     <div className='position'>
       <p>{ticker}</p>
       <p>{price}</p>
-      <p>{shares}</p>
+      <p>{weight}%</p>
+      <p>
+        {shares} share{shares > 1 ? 's' : ''}
+      </p>
     </div>
   );
 }
