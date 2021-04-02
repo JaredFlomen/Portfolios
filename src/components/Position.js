020b366704e69c094f8b701fdd1a942f9b043602
Jared Flomen <jaredflomen@gmail.com>
2021-04-02 15:22:57 -0400

    Commenting out refactored code

--- src/components/Position.js
+++ src/components/Position.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { Table } from 'react-bootstrap';
-import '../styles/position.css';
+// import React from 'react';
+// import { Table } from 'react-bootstrap';
+// import '../styles/position.css';
 
-function Position({ item }) {
-  const { ticker, price, weight, shares } = item;
-  return (
-    <Table striped bordered hover>
-      <tbody>
-        <tr>
-          <td>{ticker}</td>
-          <td>{price}</td>
-          <td>{weight}%</td>
-          <td>
-            {shares} share{shares > 1 ? 's' : ''}
-          </td>
-        </tr>
-      </tbody>
-    </Table>
-  );
-}
+// function Position({ item }) {
+//   const { ticker, price, weight, shares } = item;
+//   return (
+//     <Table striped bordered hover>
+//       <tbody>
+//         <tr>
+//           <td>{ticker}</td>
+//           <td>{price}</td>
+//           <td>{weight}%</td>
+//           <td>
+//             {shares} share{shares > 1 ? 's' : ''}
+//           </td>
+//         </tr>
+//       </tbody>
+//     </Table>
+//   );
+// }
 
-export default Position;
+// export default Position;
