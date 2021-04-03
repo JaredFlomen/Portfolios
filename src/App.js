bc64e532d0815af8bdf10c6002c7ee50150580bc
Jared Flomen <jaredflomen@gmail.com>
2021-04-03 12:46:08 -0400

    Styling the buttons

--- src/App.js
+++ src/App.js
@@ -93,16 +93,24 @@ function App() {
           </Col>
         </Row>
       </Form.Group>
-      <Button
-        disabled={allocated >= 100 ? true : false}
-        onClick={addPosition}
-        variant={allocated >= 100 ? 'secondary' : 'primary'}
-      >
-        {allocated >= 100 ? 'Fully Allocated' : 'Add A Position'}
-      </Button>
-      <Button onClick={updatePortfolio} variant='primary'>
-        Update All Positions
-      </Button>
+      <Form.Group>
+        <Row>
+          <Col className='button'>
+            <Button
+              disabled={allocated >= 100 ? true : false}
+              onClick={addPosition}
+              variant={allocated >= 100 ? 'secondary' : 'primary'}
+            >
+              {allocated >= 100 ? 'Fully Allocated' : 'Add A Position'}
+            </Button>
+          </Col>
+          <Col className='button'>
+            <Button onClick={updatePortfolio} variant='primary'>
+              Update All Positions
+            </Button>
+          </Col>
+        </Row>
+      </Form.Group>
       <div>
         {/* <Header /> */}
         {/* {positions} */}
