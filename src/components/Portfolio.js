e8632e9bcc216c5d063bc2d1fc7ba79b694541b2
Jared Flomen <jaredflomen@gmail.com>
2021-04-04 15:51:58 -0400

    Update Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Table } from 'react-bootstrap';
-// import Header from './Header';
+import Table from 'react-bootstrap/Table';
 
 function Portfolio({ portfolio }) {
   return (
