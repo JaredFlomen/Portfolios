a1e5f4db8e545177a27b06c1455238141ec95bff
Jared Flomen <jaredflomen@gmail.com>
2021-04-08 13:18:27 -0400

    Moving everything over

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
 import { Button, Table } from 'react-bootstrap';
+import Position from './Position';
 
 function Portfolio({ portfolio, deleteStock }) {
-  const [edit, setEdit] = useState(true);
-  const [newWeight, setNewWeight] = useState('');
-  // console.log({ newWeight });
   return (
     <Table striped bordered hover>
       <thead>
@@ -14,46 +12,12 @@ function Portfolio({ portfolio, deleteStock }) {
           <th>Weight</th>
           <th>Shares</th>
           <th>Delete</th>
-          <th>{edit ? 'Edit' : 'Save'}</th>
+          <th>Edit</th>
         </tr>
       </thead>
       <tbody>
         {portfolio.map((position, index) => {
-          return (
-            <tr key={index}>
-              <td>{position.ticker}</td>
-              <td>{position.price}</td>
-              {edit ? (
-                <td>{position.weight}%</td>
-              ) : (
-                <td>
-                  <input
-                    placeholder='Edit'
-                    type='text'
-                    pattern='[0-9]*'
-                    defaultValue={position.weight}
-                    onChange={e => setNewWeight(e.target.value)}
-                  />
-                </td>
-              )}
-              <td>
-                {position.shares} share{position.shares > 1 ? 's' : ''}
-              </td>
-              <td>
-                <Button
-                  variant='danger'
-                  onClick={() => deleteStock(position.ticker)}
-                >
-                  Delete
-                </Button>
-              </td>
-              <td>
-                <Button variant='secondary' onClick={() => setEdit(!edit)}>
-                  {edit ? 'Edit' : 'Save'}
-                </Button>
-              </td>
-            </tr>
-          );
+          return <Position position={position} index={index} />;
         })}
       </tbody>
     </Table>
