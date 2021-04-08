b2fbe8991943e74873f786dc368285c767def02b
Jared Flomen <jaredflomen@gmail.com>
2021-04-08 13:16:40 -0400

    Create position component

---
+++ src/components/Position.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+function Position() {
+  const [edit, setEdit] = useState(true);
+  const [newWeight, setNewWeight] = useState('');
+  // console.log({ newWeight });
+  return <div></div>;
+}
+
+export default Position;
