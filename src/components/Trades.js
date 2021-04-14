8548cb044eb2566a76f005f6d2db439edf0ef889
Jared Flomen <jaredflomen@gmail.com>
2021-04-14 11:28:18 -0400

    Viewing the trades

--- src/components/Trades.js
+++ src/components/Trades.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 function Trades({ trades }) {
-  return trades.map(i => <p>{i}</p>);
+  return trades.map((position, index) => (
+    <div key={index}>
+      <p>Ticker: {position.ticker}</p>
+      <p>Old price: {position.oldPrice}</p>
+      <p>New price: {position.newPrice}</p>
+    </div>
+  ));
 }
 
 export default Trades;
