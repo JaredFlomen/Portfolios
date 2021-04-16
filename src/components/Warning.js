9a736b37f9e8a602207c1da6ff2e5bc25b063dc6
Jared Flomen <jaredflomen@gmail.com>
2021-04-16 11:41:52 -0400

    Danger warning to warning label

--- src/components/Warning.js
+++ src/components/Warning.js
@@ -1,5 +1,11 @@
+import Alert from 'react-bootstrap/Alert';
+
 function Warning() {
-  return <div>Over-Allocated!</div>;
+  return (
+    <Alert variant='danger'>
+      <b>Over-Allocated! Please alter your position's weights</b>
+    </Alert>
+  );
 }
 
 export default Warning;
